9925852f57d03d552899b05e34100dcc66e3964e
Jason Clegg <jgcdeveloper@gmail.com>
2016-10-28 03:22:10 +0200

    Trying to Sync up NewRoom

--- app/scripts/controllers/ChatroomBarCtrl.js
+++ app/scripts/controllers/ChatroomBarCtrl.js
@@ -1,10 +1,28 @@
 (function() {
-  function ChatroomBarCtrl(Room) {
+  function ChatroomBarCtrl(Room, $uibModal) {
 
-    this.rooms = Room.all;
+    var $ctrl = this;
+
+    $ctrl.open =  function(size, parentSelector){
+
+      var modalInstance = $uibModal.open({
+        animation: $ctrl.animationsEnabled,
+        templateUrl: '/templates/newchatroom.html',
+        controller: 'NewChatroomCtrl',
+        controllerAs: 'NewChatroom',
+        resolve: {
+          newRoom: function () {
+          return $ctrl.newRoom;
+          }
+        }
+      });
+    };
+
+    $ctrl.myRooms = Room.all; //Create a Firebase room array to list during ng-repeat
+
   }
 
   angular
     .module('blocChat')
-    .controller( 'ChatroomBarCtrl', [ 'Room' , ChatroomBarCtrl ]);
+    .controller( 'ChatroomBarCtrl', [ 'Room' , '$uibModal', ChatroomBarCtrl ]);
 })();
--- app/scripts/controllers/NewChatroomCtrl.js
+++ app/scripts/controllers/NewChatroomCtrl.js
@@ -1,9 +1,19 @@
 (function() {
-  function NewChatroomCtrl(Room) {
+  function NewChatroomCtrl(Room, $uibModalInstance) {
 
+    var $ctrl = this;
+
+
+    $ctrl.save = function () {
+      $uibModalInstance.close($ctrl.newRoom);
+    };
+
+    $ctrl.cancel = function () {
+      $uibModalInstance.dismiss("cancel");
+    };
   }
 
 angular
   .module('blocChat')
-  .controller( 'NewChatroomCtrl', [ 'Room' , '$uibModal', NewChatroomCtrl ]);
+  .controller( 'NewChatroomCtrl', [ 'Room', '$uibModalInstance', NewChatroomCtrl ]);
 })();
